9d73fe5052a0bb9fe54ed72086ed7b0d5541715d
MSK <msk123@foxmail.com>
2025-11-10 21:25:54 +0800

    feat(node): :sparkles: add unit test for cpu collector

---
+++ packages/node/src/__test__/collector/cpu.spec.ts
@@ -0,0 +1,53 @@
+// biome-ignore lint/complexity/useLiteralKeys: 测试需要访问私有属性
+import { CPUCollector } from '../../collector/cpu'
+
+describe('CPUCollector', () => {
+	beforeEach(() => {
+		jest
+			.spyOn(process, 'cpuUsage') // 单位：微妙
+			.mockImplementationOnce(() => ({
+				user: 1000,
+				system: 2000,
+			})) // init时第一次获取CPU使用率
+			.mockImplementationOnce(() => ({
+				user: 2000,
+				system: 4000,
+			})) // get时第二次获取CPU使用率
+		jest
+			.spyOn(process.hrtime, 'bigint') // 单位：纳秒
+			.mockImplementationOnce(() => 1000000000n) // init时第一次获取当前时间
+			.mockImplementationOnce(() => 2000000000n) // get时第二次获取当前时间
+	})
+
+	afterEach(() => {
+		jest.restoreAllMocks()
+	})
+
+	describe('get', () => {
+		it('should calculate CPU usage correctly', () => {
+			const cpuCollector = new CPUCollector()
+			expect(cpuCollector.get()).toEqual({
+				load: 0.3,
+				useLoad: 0.1,
+			})
+		})
+	})
+
+	describe('destroy', () => {
+		it('should reset internal state', () => {
+			const cpuCollector = new CPUCollector()
+			expect(cpuCollector['_lastHrtime']).toBe(1000000000n)
+			expect(cpuCollector['_lastCpuUsage']).toEqual({
+				user: 1000,
+				system: 2000,
+			})
+			expect(cpuCollector['_teardown']).toHaveLength(0)
+			cpuCollector.destroy()
+			expect(cpuCollector['_lastHrtime']).toBe(0n)
+			expect(cpuCollector['_lastCpuUsage']).toEqual({
+				user: 0,
+				system: 0,
+			})
+		})
+	})
+})
